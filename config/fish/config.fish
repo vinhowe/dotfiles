6637c7626d25dce7d49e445db0cab76b8074c896
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2020-09-23 12:45:27 -0600

    Use gotham theme with shell

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,77 +1,7 @@
 fenv source ~/.profile
+fenv source ~/.nvm/nvm.sh
 
-function l
-    command ls --color -CF $argv
-end
-
-function ..
-    builtin cd $argv ..
-end
-
-function lj
-	builtin cd /home/vin/dev/lifesystem/journal; . ../venv/bin/activate.fish; python lj.py $argv; deactivate; cd -
-end
-
-function sj
-	builtin cd /home/vin/dev/lifesystem/spec; . ../venv/bin/activate.fish; python sj.py $argv; deactivate; cd -
-end
-
-function per
-    builtin cd /home/vin/dev/lifesystem; . venv/bin/activate.fish; python app.py s; deactivate; cd -
-end
-
-function jour
-    builtin cd /home/vin/dev/lifesystem; . venv/bin/activate.fish; python app.py p j; deactivate; cd -
-end
-
-function plan 
-    builtin cd /home/vin/dev/lifesystem; . venv/bin/activate.fish; python app.py p $argv; deactivate; cd -
-end
-
-function log 
-    builtin cd /home/vin/dev/lifesystem; . venv/bin/activate.fish; python log_app.py; deactivate; cd -
-end
-
-function cl
-    setxkbmap -option caps:swapescape
-    xdotool key Caps_Lock
-end
-
-function rules
-    lj -d rules
-end
-
-function cms
-	builtin cd /home/vin/dev/website/blog; gatsby develop; cd -
-end
-
-function python
-    command python3 $argv
-end
-
-function emacs
-    command emacs -nw $argv
-end
-
-function te
-    command touch $argv; typora $argv
-end
-
-function brb
-    command fish -c "cd ~/dev/berightback/; ./fullscreen_lock.sh"
-end
-
-function db
-    command ~/disconnect_bluetooth.sh
-end
-
-function cb
-    command ~/u8i_connect.sh
-end
-
-bass source '/home/vin/Downloads/google-cloud-sdk/path.bash.inc'
-
-bass source '/home/vin/Downloads/google-cloud-sdk/completion.bash.inc'
+eval sh $HOME/.config/gotham/gotham.sh
 
 set fish_greeting
 
@@ -81,14 +11,26 @@ function fish_user_key_bindings
     for mode in insert default visual
         bind -M $mode \cf forward-char
     end
-
-    fish_default_key_bindings -M default
-
-    fish_vi_key_bindings default
 end
 
+fish_vi_key_bindings
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /home/vin/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
+function per
+    builtin cd /home/vinhowe/dev/lifesystem; . venv/bin/activate.fish; python app.py s; deactivate
+end
+
+function icat
+    kitty +kitten icat --align=left $argv
+end
+
+function vi
+    nvim $argv
+end
+
+function jour
+    builtin cd /home/vinhowe/dev/lifesystem; . venv/bin/activate.fish; python app.py p j; deactivate
+end
+
+function plan 
+    builtin cd /home/vinhowe/dev/lifesystem; . venv/bin/activate.fish; python app.py p $argv; deactivate
+end
