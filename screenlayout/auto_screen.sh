c3340777cdbf2851a0580a7b51e86ec7b3c3b175
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2020-11-01 22:03:09 -0700

    Use same scale for both modes

--- screenlayout/auto_screen.sh
+++ screenlayout/auto_screen.sh
@@ -13,20 +13,19 @@ echo [$(date -u)] "attempting to automatically set screen output settings"
 export DISPLAY=:0
 export XAUTHORITY=$HOME/.Xauthority
 
+echo "Xft.dpi: 192" > ~/.Xresources
+export GDK_SCALE=2
+export GDK_DPI_SCALE=0.5
+
 if [ "$HDMI_STATUS" == "connected" ]
 then
     echo [$(date)] "setting mode to docked"
     echo [$(date -u)] "attempting to set HDMI1 settings"
 
-    # xrandr --auto && xrandr --output HDMI1 --primary --mode 2560x1440 --pos 0x0 --output eDP1 --off
-    # xrandr --auto && xrandr --output HDMI1 --primary --mode 2560x1440 --dpi 192 --rotate normal --pos 0x0
     xrandr --output HDMI1 --auto --left-of eDP1
     xrandr --output HDMI1 --primary --mode 3840x2160 --scale 1x1 --dpi 192 --rotate normal --pos 0x0 --verbose
     xrandr --output eDP1 --right-of HDMI1 --auto --scale 2x2 --verbose
 
-    echo "Xft.dpi: 192" > ~/.Xresources
-    export GDK_SCALE=2
-    export GDK_DPI_SCALE=0.5
 
     if [ "$1" == "w" ]
     then
@@ -35,16 +34,12 @@ then
         i3-msg [workspace=0] move workspace to output eDP1
     fi
 
-    /usr/bin/notify-send "HDMI active" "Switching to docked mode"
+    notify-send "HDMI active" "Switching to docked mode"
 else
     echo [$(date -u)] "setting mode to mobile"
 
     xrandr --auto && xrandr --output HDMI1 --off
-    xrandr --output eDP1 --primary --dpi 96 --mode 1366x768 --scale 1x1
-
-    echo "Xft.dpi: 96" > ~/.Xresources
-    export GDK_SCALE=1
-    export GDK_DPI_SCALE=1
+    xrandr --output eDP1 --primary --dpi 192 --mode 1366x768 --scale 2x2
 
     if [ "$1" == "w" ]
     then
@@ -52,7 +47,7 @@ else
         i3-msg workspace 0
     fi
 
-    /usr/bin/notify-send "HDMI inactive" "Switching to mobile mode"
+    notify-send "HDMI inactive" "Switching to mobile mode"
 fi
 
 i3-msg reload
