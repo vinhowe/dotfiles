f0823ac329cd8e2106bfd410706db1025afb7d3f
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2020-12-04 00:57:59 -0700

    Get rid of a bunch of fish alias cruft

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,25 +11,13 @@ set -gx GPG_TTY (tty)
 function l
     command ls --color -CF $argv
 end
+
 function ..
     builtin cd $argv ..
 end
 
-function lj
-    builtin cd /home/vin/dev/lifesystem/journal; . ../venv/bin/activate.fish; python lj.py $argv; deactivate; cd -
-end
-
-function sj
-    builtin cd /home/vin/dev/lifesystem/spec; . ../venv/bin/activate.fish; python sj.py $argv; deactivate; cd -
-end
-
 function per
     builtin cd /home/vin/dev/lifesystem; . venv/bin/activate.fish; python app.py s; deactivate; cd -
-end
-eval sh $HOME/.config/gotham/gotham.sh
-
-function jour
-    builtin cd /home/vin/dev/lifesystem; . venv/bin/activate.fish; python app.py p j; deactivate; cd -
 end
 
 # Thanks http://christopherroach.com/articles/jupyterlab-desktop-app/
@@ -47,25 +35,13 @@ function plan
     builtin cd /home/vin/dev/lifesystem; . venv/bin/activate.fish; python app.py p $argv; deactivate; cd -
 end
 
-function log
-    builtin cd /home/vin/dev/lifesystem; . venv/bin/activate.fish; python log_app.py; deactivate; cd -
-end
-
 function cl
     setxkbmap -option caps:swapescape
     xdotool key Caps_Lock
 end
 
-function rules
-    lj -d rules
-end
-
 function cms
 	builtin cd /home/vin/dev/website/blog; gatsby develop; cd -
-end
-
-function python
-    command python3 $argv
 end
 
 function emacs
@@ -79,18 +55,6 @@ end
 function brb
     command fish -c "cd ~/dev/berightback/; ./fullscreen_lock.sh"
 end
-
-function db
-    command ~/disconnect_bluetooth.sh
-end
-
-function cb
-    command ~/u8i_connect.sh
-end
-
-bass source '/home/vin/Downloads/google-cloud-sdk/path.bash.inc'
-
-bass source '/home/vin/Downloads/google-cloud-sdk/completion.bash.inc'
 
 set fish_greeting
 
@@ -108,15 +72,6 @@ end
 
 function icat
     kitty +kitten icat --align=left $argv
-end
-
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /home/vin/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-function vi
-    nvim $argv
 end
 
 function til
