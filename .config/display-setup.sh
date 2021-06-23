a9da2e4c0cc1a9edbd33183bf29c130f83e6eca7
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-06-23 15:51:24 -0600

    Custom align displays

--- .config/display-setup.sh
+++ .config/display-setup.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-xrandr --output DP-0 --primary
-xrandr --output DP-2 --rotate left --left-of DP-0
-xrandr --output HDMI-0 --right-of DP-0
+xrandr --output HDMI-0 --off --output DP-0 --primary --mode 2560x1440 --pos 1440x596 --rotate normal --output DP-1 --off --output DP-2 --mode 2560x1440 --pos 0x0 --rotate left --output DP-3 --off --output DP-4 --off --output DP-5 --off
