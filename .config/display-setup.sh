1479bb8f26364acba995ea25e8c2c35930d75957
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-07-11 00:50:42 -0600

    Update naga display alignment

--- .config/display-setup.sh
+++ .config/display-setup.sh
@@ -1,3 +1,2 @@
-#!/usr/bin/env bash
-
-xrandr --output HDMI-0 --off --output DP-0 --primary --mode 2560x1440 --pos 1440x596 --rotate normal --output DP-1 --off --output DP-2 --mode 2560x1440 --pos 0x0 --rotate left --output DP-3 --off --output DP-4 --off --output DP-5 --off
+#!/bin/sh
+xrandr --output DVI-I-0 --off --output DVI-I-1 --off --output HDMI-0 --mode 3840x2160 --pos 4000x200 --rotate normal --output DP-0 --primary --mode 2560x1440 --pos 1440x596 --rotate normal --output DP-1 --off --output DP-2 --mode 2560x1440 --pos 0x0 --rotate left --output DP-3 --off --output DP-4 --off --output DP-5 --off
